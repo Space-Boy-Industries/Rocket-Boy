65651c3520a0559ac26b6a618f4c13485a9c2784
Eric Sexton <foopis23@gmail.com>
2020-01-31 22:56:08 -0500

    animations implemented

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
-function love.load()
+local animation = require "lib/animation";
 
+local test = nil;
+
+function love.load()
+    test = animation.loadAnimation("test");
 end
 
 function love.update(dt)
-
+    if test ~= nil then
+        test = animation.updateAnimation(test, dt);
+    end
 end
 
 function love.draw()
-    love.graphics.print("Hello World", 400, 300);
+    local frame = test["frame"];
+    love.graphics.draw(test["frames"][frame], 1, 1, 0, 0.5, 0.5);
 end
